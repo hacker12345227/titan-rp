23e82949126e35e59a8ae0e2193419711b34a0db
hacker12345227 <Jarno.eulen12@gmail.com>
2026-04-03 23:31:27 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
 function joinServer() {
-  window.location.href = "fivem://connect/your-ip";
+window.location.href = "fivem://connect/your-ip";
 }
+
+// Loader
+window.addEventListener("load", () => {
+document.getElementById("loader").style.display = "none";
+});
+
+// Scroll animations
+const elements = document.querySelectorAll('.fade-up');
+
+window.addEventListener('scroll', () => {
+elements.forEach(el => {
+const top = el.getBoundingClientRect().top;
+if (top < window.innerHeight - 100) {
+el.classList.add('show');
+}
+});
+});
+
+// Cursor glow
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", e => {
+glow.style.left = e.clientX + "px";
+glow.style.top = e.clientY + "px";
+});
+
+// Fake player counter (later vervangen met API)
+let count = 0;
+setInterval(() => {
+if (count < 128) {
+count++;
+document.getElementById("players").innerText = count;
+}
+}, 50);
